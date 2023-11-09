d6e6c668da3221a7c38bc92538ca988dee11899c
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2023-11-09 19:42:17 +0100

    committed

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      hi what are you doing
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/navBar";
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div className="">
+        <img src="../images/banner-home1.png" alt="banner"  ></img>
+        
+      </div>
+    </div>
+  );
+}
