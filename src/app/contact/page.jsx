d065b9142075e9901b2a7dac7208c9d3fe3672c3
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2023-11-12 11:42:18 +0100

    committed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,14 @@
 import NavBar from "@/components/navBar";
-import style from "./contact.css";
+import style from "./contact.module.css";
 import React from "react";
 
 const Contact = () => {
   return (
     <div>
       <NavBar />
-      <div id={style.banner_contact}></div>
+      <div id={style.banner_contact}>
+        <h2>Contact</h2>
+      </div>
       {/* <img src="../images/banner-contact.png" alt="banner-contact"></img> */}
     </div>
   );
